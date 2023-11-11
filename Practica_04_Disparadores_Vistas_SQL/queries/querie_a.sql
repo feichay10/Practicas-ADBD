e5a59c6c48332bea7d8d72f16fe1a9c4a37de2dc
feichay10 <alu0101364544@ull.edu.es>
2023-11-11 02:52:30 +0000

    Done exercise 4

---
+++ Practica_04_Disparadores_Vistas_SQL/queries/querie_a.sql
@@ -0,0 +1,10 @@
+CREATE VIEW films_list AS
+SELECT category.name, SUM(payment.amount) AS total_sales
+FROM category
+INNER JOIN film_category ON category.category_id = film_category.category_id
+INNER JOIN film ON film_category.film_id = film.film_id
+INNER JOIN inventory ON film.film_id = inventory.film_id
+INNER JOIN rental ON inventory.inventory_id = rental.inventory_id
+INNER JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY category.name
+ORDER BY total_sales DESC;
