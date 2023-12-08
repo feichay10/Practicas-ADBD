babf878c50e40923e8b1c23a79aea449d0336706
feichay10 <alu0101364544@ull.edu.es>
2023-12-08 23:46:22 +0000

    Starting activity 2

---
+++ Practica_06_API_Rest_Flask/actividad_2/room_app.py
@@ -0,0 +1,36 @@
+﻿import os
+import psycopg2
+import json
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database="myhome",
+                            # user=os.environ['DB_USERNAME'],
+                            user="postgres",
+                            # password=os.environ['DB_PASSWORD']
+                            password="clave")
+    return conn
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM public.rooms;')
+    values = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', values=values)
+
+
+@app.route('/about/', methods=('GET', 'POST'))
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '_main_':
+    app.run(host='0.0.0.0', port=8080, debug=True)
