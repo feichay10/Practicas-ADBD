6bb79dec745e2f6a5c6556c2b084956ac067b744
feichay10 <alu0101364544@ull.edu.es>
2023-12-08 16:28:35 +0000

    Minor fixes

--- Practica_06_API_Rest_Flask/practica_flask/app.py
+++ Practica_06_API_Rest_Flask/practica_flask/app.py
@@ -46,7 +46,27 @@ def create():
 
     return render_template('create.html')
 
+@app.route('/delete/', methods=['GET', 'POST'])
+def delete():
+    if request.method == 'POST':
+        id = request.form['ID']
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('DELETE FROM books WHERE id = %s',
+                    (id))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('delete.html')
+
 
 @app.route('/about/', methods=('GET', 'POST'))
 def about():
     return render_template('about.html')
+
+
+if __name__ == '_main_':
+    app.run(host='0.0.0.0', port=8080, debug=True)
