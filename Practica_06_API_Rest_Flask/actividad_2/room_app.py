886f6e7b3c2568579ec76142d603cb8797674a7a
feichay10 <alu0101364544@ull.edu.es>
2023-12-09 18:46:18 +0000

    Done practice but have to do exceptions

--- Practica_06_API_Rest_Flask/actividad_2/room_app.py
+++ Practica_06_API_Rest_Flask/actividad_2/room_app.py
@@ -57,12 +57,50 @@ def name_room():
         id = request.form['ID']
         conn = get_db_connection()
         cur = conn.cursor()
-        cur.execute('SELECT name FROM public.rooms WHERE id = %s;', (id))
+        cur.execute('SELECT name FROM public.rooms WHERE id = ' + id + ';')
         values = cur.fetchall()
         cur.close()
         conn.close()
         return render_template('result_name_room.html', rooms=values)
     return render_template('name_room.html')
+
+
+@app.route('/room_avg_temperature/', methods=('GET', 'POST'))
+def room_avg_temperature():
+    if request.method == 'POST':
+        id = request.form['ID']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(
+            'SELECT AVG(temperature) FROM temperatures WHERE room_id = ' + id + ';')
+        values = cur.fetchall()
+        cur.close()
+        conn.close()
+        return render_template('result_room_avg_temperature.html', rooms=values)
+    return render_template('room_avg_temperature.html')
+
+
+@app.route('/room_min_temperature/', methods=('GET', 'POST'))
+def room_min_temperature():
+    if request.method == 'POST':
+        id = request.form['ID']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT MIN(temperature), r.name '
+                    'FROM public.rooms r '
+                    'JOIN public.temperatures t ON r.id = t.room_id '
+                    'WHERE room_id = ' + id + 'GROUP BY(room_id, name)')
+        values = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        result = {
+            'room_name': values[0],
+            'min_temperature': values[1]
+        }
+
+        return render_template('result_room_min_temperature.html', room=result)
+    return render_template('room_min_temperature.html')
 
 
 @app.route('/about/', methods=('GET', 'POST'))
