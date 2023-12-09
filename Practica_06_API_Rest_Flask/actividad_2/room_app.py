dd897fafcf9a49ad95ce038744b50381c806c2ec
feichay10 <alu0101364544@ull.edu.es>
2023-12-09 10:34:42 +0000

    Done exercise 3

--- Practica_06_API_Rest_Flask/actividad_2/room_app.py
+++ Practica_06_API_Rest_Flask/actividad_2/room_app.py
@@ -51,6 +51,20 @@ def maximum():
     return render_template('maximum.html', rooms=values)
 
 
+@app.route('/name_room/', methods=('GET', 'POST'))
+def name_room():
+    if request.method == 'POST':
+        id = request.form['ID']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT name FROM public.rooms WHERE id = %s;', (id))
+        values = cur.fetchall()
+        cur.close()
+        conn.close()
+        return render_template('result_name_room.html', rooms=values)
+    return render_template('name_room.html')
+
+
 @app.route('/about/', methods=('GET', 'POST'))
 def about():
     return render_template('about.html')
